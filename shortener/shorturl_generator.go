9d5ccb3739cbd5ea9cca6dac968110007ad9221a
Gendis Jawi <gendisjawi44@gmail.com>
2023-12-12 07:16:37 +0000

    feat: Add base58-encoded function
    Added base58-encoded function for shortening URLs.

--- shortener/shorturl_generator.go
+++ shortener/shorturl_generator.go
@@ -2,10 +2,17 @@ package shortener
 
 import (
 	"crypto/sha256"
+	"fmt"
+	"github.com/itchyny/base58-go"
+	"os"
 )
 
 func sha2560f(input stirng) []byte {
 	algorithm := sha256.New()
 	algorithm.Write([]byte(input))
 	return algorithm.Sum(nil)
+}
+
+func base58Encoded(bytes []byte) string {
+
 }

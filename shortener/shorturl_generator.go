5c5cba8811306e57b00ee90f915c7b82410750d8
Gendis Jawi <gendisjawi44@gmail.com>
2023-12-12 07:23:09 +0000

    feat: Add base58 encoding for short URL generation
    Add base58 encoding for short URL generation using github.com/itchyny/base58-go library

--- shortener/shorturl_generator.go
+++ shortener/shorturl_generator.go
@@ -3,8 +3,9 @@ package shortener
 import (
 	"crypto/sha256"
 	"fmt"
-	"github.com/itchyny/base58-go"
 	"os"
+
+	"github.com/itchyny/base58-go"
 )
 
 func sha2560f(input string) []byte {
@@ -14,5 +15,12 @@ func sha2560f(input string) []byte {
 }
 
 func base58Encoded(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(encoded)
 
 }

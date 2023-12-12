a4201b0f8e6a39a355c95f0756b01db57ed479fe
Gendis Jawi <gendisjawi44@gmail.com>
2023-12-12 06:49:23 +0000

    feat: Add Redis storage functionality
    Added a StorageService and functions to save and retrieve URL mappings using Redis

---
+++ store/store_service.go
@@ -0,0 +1,53 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis"
+	"time"
+)
+
+type StorageService struct {
+	redisClient *redis.Client
+}
+
+var (
+	storeService = &StorageService{}
+	ctx = context.Background()
+)
+
+const CacheDuration = 6 * time.Hour
+
+func InitializeStore() *StorageService {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379".
+		Password: "",
+		DB: 0,
+	})
+
+	pong, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Error init Redis: %v", err))
+	}
+
+	fmt.Printf("\nRedis started successfully: pong message = {%s}", pong)
+	storeService.redisClient = redisClient
+	return storeService
+}
+
+func SaveUrlMapping(shortUrl string, originalUrl string, userId string) {
+	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, userId, CacheDuration).Err()
+	if err != nil {
+		panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, originalUrl))
+	}
+
+}
+
+func RetrieveInitialUrl(shortUrl string) string {
+	result, err := storeService.redisClient.Get(ctx, shortUrl).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Failed RetrieveInitialUrl | Error: %v - shortUrl: %s\n", err, shortUrl))
+	}
+	return result
+
+}

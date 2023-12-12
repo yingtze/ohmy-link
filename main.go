d9624afd5a2ba75daaa66fa4f07a48cc50015d0f
CodeSandbox <support@codesandbox.io>
2023-12-12 06:23:24 +0000

    Initial commit

--- main.go
+++ main.go
@@ -7,5 +7,14 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/")
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hey Go URL Shortener 🚀 !",
+		})
+	})
+
+	err := r.Run(":3000")
+	if err != nil {
+		panic(fmt.Sprintf("failed to start the web server - Error: %v", err))
+	}
 }
